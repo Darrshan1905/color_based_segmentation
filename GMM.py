8b60dd0725e74c888fd0e69d80cdf3632d86e722
Darrshan1905 <darrshan@mobicip.com>
2024-05-14 12:28:15 +0530

    added comments and found optimal k value using silhoutte scores and elbow method

--- GMM.py
+++ GMM.py
@@ -3,15 +3,40 @@ import cv2
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
 
+#input the image
 image = cv2.imread('./bubbles.png')
-
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+#reshape the image to a standard format (number of pixels as rows and 3 columns for rgb)
 reshaped_image = image.reshape((-1, 3))
 
-num_clusters = 5
+# Define a range of values for k
+min_components = 1
+max_components = 20
+n_components = np.arange(min_components, max_components)
 
-gmm = GaussianMixture(n_components=num_clusters, random_state=42)
+log_likelihoods = []
+
+for k in n_components:
+    gmm = GaussianMixture(n_components=k, random_state=42)
+    gmm.fit(reshaped_image)
+    
+    log_likelihoods.append(gmm.score(reshaped_image))
+
+plt.plot(n_components, log_likelihoods, marker='o')
+plt.xlabel('Number of components')
+plt.ylabel('Log Likelihood')
+plt.title('Log Likelihood vs. Number of Components')
+plt.grid(True)
+plt.show()
+
+# Find the optimal number of components
+diffs = np.diff(log_likelihoods)
+elbow_point = np.where(diffs < 0)[0][0] + 2
+print("Elbow point (optimal number of components):", elbow_point)
+
+# Segmentation using GMM with optimal K
+gmm = GaussianMixture(n_components=elbow_point, random_state=42)
 gmm.fit(reshaped_image)
 
 labels = gmm.predict(reshaped_image)
--- kmeans.py
+++ kmeans.py
@@ -3,28 +3,28 @@ import numpy as np # type: ignore
 import matplotlib.pyplot as plt # type: ignore
 import cv2 # type: ignore
 from sklearn.cluster import KMeans # type: ignore
+from sklearn.metrics import silhouette_score
 
+#input the image
 image = cv2.imread(r'./bubbles.png', cv2.IMREAD_UNCHANGED)
-
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.axis('off')
 plt.show()
 
-
+#Convert image to gray
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap = "gray")
 plt.axis('off')
 plt.show()
 
-
+#Apply binary threshold and erode:
 _ , mask = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)
 mask = cv2.erode(mask, np.ones((7, 7), np.uint8))
 plt.imshow(mask, cmap = "gray")
 plt.axis('off')
-# cv2.imwrite('thresholding.png', cv2.hconcat([image, np.stack((mask, mask, mask), axis=2)]))
 plt.show()
 
-
+#Find contours and display all contours
 contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 contours_img_before_filtering = mask.copy()
@@ -34,16 +34,16 @@ plt.imshow(contours_img_before_filtering)
 plt.axis('off')
 plt.show()
 
-
+# Filter contours and extract mean colors
 filtered_contours = []
 df_mean_color = pd.DataFrame()
 for idx, contour in enumerate(contours):
     area = int(cv2.contourArea(contour))
     # print("Contour", idx, "Area:", area)
 
-    # if area is higher than 110:
     if area > 110:
         filtered_contours.append(contour)
+        
         # get mean color of contour:
         masked = np.zeros_like(image[:, :, 0])
         cv2.drawContours(masked, [contour], 0, 255, -1)
@@ -52,19 +52,48 @@ for idx, contour in enumerate(contours):
         df = pd.DataFrame({'B_mean': B_mean, 'G_mean': G_mean, 'R_mean': R_mean}, index=[idx])
         df_mean_color = pd.concat([df_mean_color, df])
 
-print(df_mean_color)
 
+# Display filtered contours
 contours_img_after_filtering = mask.copy()
 contours_img_after_filtering = cv2.cvtColor(contours_img_after_filtering, cv2.COLOR_GRAY2BGR)
 cv2.drawContours(contours_img_after_filtering, tuple(filtered_contours), -1, (0, 255, 0), 3)
-
 plt.imshow(contours_img_after_filtering)
 plt.axis('off')
 plt.show()
-
 cv2.imwrite('contours.png', cv2.hconcat([contours_img_before_filtering, contours_img_after_filtering]))
 
-km = KMeans( n_clusters=6)
+print(df_mean_color)
+
+
+features = df_mean_color[['B_mean', 'G_mean', 'R_mean']]
+
+# Define a range of values for k
+min_clusters = 2
+max_clusters = 10
+n_clusters_range = range(min_clusters, max_clusters + 1)
+
+silhouette_scores = []
+
+# Iterate through different values of k
+for n_clusters in n_clusters_range:
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    cluster_labels = kmeans.fit_predict(features)
+    silhouette_avg = silhouette_score(features, cluster_labels)
+    silhouette_scores.append(silhouette_avg)
+
+plt.plot(n_clusters_range, silhouette_scores, marker='o')
+plt.xlabel('Number of clusters (K)')
+plt.ylabel('Silhouette Score')
+plt.title('Silhouette Score vs. Number of Clusters')
+plt.grid(True)
+plt.show()
+
+# Find the optimal number of clusters (K)
+optimal_k = n_clusters_range[np.argmax(silhouette_scores)]
+print("Optimal number of clusters (K):", optimal_k)
+
+# Segmentation using K-means with optimal K
+km = KMeans( n_clusters=optimal_k)
 df_mean_color['label'] = km.fit_predict(df_mean_color)
 
 def draw_segmented_objects(image, contours, label_cnt_idx, bubbles_count):
